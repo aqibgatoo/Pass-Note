f2ffbe5b71764fa13d0bc95933cbed1307baf36b
Mohd Aqib <aqib.droid@gmail.com>
2013-11-12 22:41:51 +0530

    Add dialog for the image activity

--- src/com/aqib/passnote/ImageActivity.java
+++ src/com/aqib/passnote/ImageActivity.java
@@ -1,15 +1,58 @@
 package com.aqib.passnote;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.ImageView;
 
 public class ImageActivity extends Activity {
+
+	private static final CharSequence title = "Create your Pass Point Sequence";
+	private static final CharSequence message = "Touch four points to create your pass point sequence";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_image);
+		addTouchListener();
+		showPrompt();
+	}
+
+	private void showPrompt() {
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle(title).setMessage(message)
+				.setPositiveButton("OK", new OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+
+					}
+				});
+
+		AlertDialog alertDialog = builder.create();
+		alertDialog.show();
+
+	}
+
+	private void addTouchListener() {
+
+		ImageView imageView = (ImageView) findViewById(R.id.touch_image);
+		imageView.setOnTouchListener(new OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+
+				return false;
+			}
+		});
+
 	}
 
 	@Override
