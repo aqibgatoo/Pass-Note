c53854748a18337648781e676c69a2ebf6544c16
Mohd Aqib <aqib.droid@gmail.com>
2013-12-25 14:04:13 +0530

    Add code for saving ,deleting nad updating a note

--- src/com/aqib/passnote/MainActivity.java
+++ src/com/aqib/passnote/MainActivity.java
@@ -12,29 +12,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract.CommonDataKinds.Note;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends ListActivity {
+	public static final int NOTE_EDITOR_CONSTANT = 1001;
 	public static final String LOGTAG = "MAG";
-//	private static final String KEY = "hello";
+	public static final int MENU_DELETE_ID=1002;
+	private int currentNode;
+	
+	// private static final String KEY = "hello";
 
 	// private EditText text;
 
 	private NoteItemDataSource dataSource;
 	private List<NoteItem> noteList;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-//		setButtonListener();
-		dataSource=new NoteItemDataSource(this);
-		refreshDispalay();
+		// setButtonListener();
+		registerForContextMenu(getListView());
+		dataSource = new NoteItemDataSource(this);
+		refreshDisplay();
 
 		// text = (EditText) findViewById(R.id.edit_text);
 		// SharedPreferences preferences=getPreferences(MODE_PRIVATE);
@@ -47,37 +57,38 @@ public class MainActivity extends ListActivity {
 
 	}
 
-//	private void setButtonListener() {
-//
-//		Button button = (Button) findViewById(R.id.save_button);
-//		button.setOnClickListener(new OnClickListener() {
-//
-//			@Override
-//			public void onClick(View v) {
-//
-//				// text = (EditText) findViewById(R.id.edit_text);
-//				// String value=text.getText().toString();
-//				// SharedPreferences preferences=getPreferences(MODE_PRIVATE);
-//				// SharedPreferences.Editor editor=preferences.edit();
-//				// editor.putString(KEY,value);
-//				// editor.commit();
-//
-//				// String value = getString(R.id.edit_text);
-//				//
-//				// preferences = getPreferences(MODE_PRIVATE);
-//				// SharedPreferences.Editor editor = preferences.edit();
-//				// editor.putString(KEY, value);
-//				// editor.commit();
-//
-//			}
-//		});
-//
-//	}
+	// private void setButtonListener() {
+	//
+	// Button button = (Button) findViewById(R.id.save_button);
+	// button.setOnClickListener(new OnClickListener() {
+	//
+	// @Override
+	// public void onClick(View v) {
+	//
+	// // text = (EditText) findViewById(R.id.edit_text);
+	// // String value=text.getText().toString();
+	// // SharedPreferences preferences=getPreferences(MODE_PRIVATE);
+	// // SharedPreferences.Editor editor=preferences.edit();
+	// // editor.putString(KEY,value);
+	// // editor.commit();
+	//
+	// // String value = getString(R.id.edit_text);
+	// //
+	// // preferences = getPreferences(MODE_PRIVATE);
+	// // SharedPreferences.Editor editor = preferences.edit();
+	// // editor.putString(KEY, value);
+	// // editor.commit();
+	//
+	// }
+	// });
+	//
+	// }
 
-	private void refreshDispalay() {
-	
-		noteList=dataSource.getAll();
-		ArrayAdapter<NoteItem> adapter=new ArrayAdapter<NoteItem>(this, R.layout.list_item_layout,noteList);
+	private void refreshDisplay() {
+
+		noteList = dataSource.getAll();
+		ArrayAdapter<NoteItem> adapter = new ArrayAdapter<NoteItem>(this,
+				R.layout.list_item_layout, noteList);
 		setListAdapter(adapter);
 	}
 
@@ -107,10 +118,7 @@ public class MainActivity extends ListActivity {
 			onMenuExitClick(item);
 			handled = true;
 			break;
-		case R.id.create_note:
-			createNote();
-			handled=true;
-			break;
+
 		default:
 			handled = super.onMenuItemSelected(featureId, item);
 			break;
@@ -122,13 +130,23 @@ public class MainActivity extends ListActivity {
 
 	private void createNote() {
 
-		NoteItem item=NoteItem.getNew();
-		
-		Intent intent=new Intent(this,NoteEditorActivity.class);
+		NoteItem item = NoteItem.getNew();
+
+		Intent intent = new Intent(this, NoteEditorActivity.class);
 		intent.putExtra("key", item.getKey());
-		intent.putExtra("value",item.getValue());
-		startActivityForResult(intent, 1001);
-		
+		intent.putExtra("value", item.getValue());
+		startActivityForResult(intent, NOTE_EDITOR_CONSTANT);
+
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		if (item.getItemId() == R.id.create_note) {
+			createNote();
+		}
+
+		return super.onOptionsItemSelected(item);
 	}
 
 	public void onMenuChangePictureClick(MenuItem item) {
@@ -147,5 +165,46 @@ public class MainActivity extends ListActivity {
 
 		finish();
 
+	}
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+	
+		NoteItem item=noteList.get(position);
+		Intent intent = new Intent(this, NoteEditorActivity.class);
+		intent.putExtra("key", item.getKey());
+		intent.putExtra("value", item.getValue());
+		startActivityForResult(intent, NOTE_EDITOR_CONSTANT);
+
+	}
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+		if (requestCode==NOTE_EDITOR_CONSTANT&& resultCode==RESULT_OK) {
+			NoteItem item=new NoteItem();
+			item.setKey(data.getStringExtra("key"));
+			item.setValue(data.getStringExtra("value"));
+			dataSource.update(item);
+			refreshDisplay();
+		}
+		
+	}
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		AdapterContextMenuInfo info=(AdapterContextMenuInfo) menuInfo;
+		currentNode=(int) info.id;
+	menu.add(0,MENU_DELETE_ID,0,"Delete");
+	
+	}
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+
+		if (item.getItemId()==MENU_DELETE_ID) {
+			NoteItem note=noteList.get(currentNode);
+			dataSource.remove(note);
+			refreshDisplay();
+		}
+		
+		return super.onContextItemSelected(item);
 	}
 }
--- src/com/aqib/passnote/NoteEditorActivity.java
+++ src/com/aqib/passnote/NoteEditorActivity.java
@@ -1,31 +1,60 @@
 package com.aqib.passnote;
 
 import com.aqib.passnote.data.NoteItem;
-
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.EditText;
 
+@SuppressLint("NewApi")
 public class NoteEditorActivity extends Activity {
 
-	
 	private NoteItem item;
+
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.id.textEdit);
+		setContentView(R.layout.android_editor_layout);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 
-		Intent intent=this.getIntent();
-		item=new NoteItem();
+		Intent intent = this.getIntent();
+		item = new NoteItem();
 		item.setKey(intent.getStringExtra("key"));
 		item.setValue(intent.getStringExtra("value"));
-		
-		EditText editText=(EditText) findViewById(R.id.textEdit);
+
+		EditText editText = (EditText) findViewById(R.id.textEdit);
 		editText.setText(item.getValue());
 		editText.setSelection(item.getValue().length());
-		
-		
+
+	}
+
+	public void saveAndFinish() {
+
+		EditText editText = (EditText) findViewById(R.id.textEdit);
+		String textValue = editText.getText().toString();
+
+		Intent intent = new Intent();
+		intent.putExtra("key", item.getKey());
+		intent.putExtra("value", textValue);
+		setResult(RESULT_OK, intent);
+		finish();
+
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			saveAndFinish();
+		}
+		return false;
+	}
+
+	@Override
+	public void onBackPressed() {
+		saveAndFinish();
 	}
 
 }
